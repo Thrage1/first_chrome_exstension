023a59f35d4e667e5a9d94c6ce07c54185aca783
David Veytsman <dveyts@yahoo.com>
2018-05-30 13:07:42 -0400

    linked a button to open all the links on click

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded",function(){
+var open = document.getElementById('open');
+open.addEventListener("click", function(event) {
   event.preventDefault();
   var links = document.getElementsByTagName('a');
   // debugger
@@ -12,3 +14,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
   console.log("DOM fully loaded and parsed");
 });
+});
+// document.addEventListener("DOMContentLoaded", function open(event) {
+//   event.preventDefault();
+//   var links = document.getElementsByTagName('a');
+//   // debugger
+//   for(var i = 0; i < links.length; i++){
+//     var link = links[i];
+//     chrome.tabs.create({url: link.href, selected: false})
+//     console.log(link);
+//     // link.click()
+//     // links[i].click();
+//     // console.log(links[i]);
+//   }
+//   console.log("DOM fully loaded and parsed");
+// });
